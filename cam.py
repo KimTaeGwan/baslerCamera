ea459c67e5ee7f750bab7b0af333f3a474528581
KimTaeGwan <chtaeryu@gmail.com>
2023-05-02 15:49:22 +0900

    modify

--- cam.py
+++ cam.py
@@ -1,41 +1,42 @@
 from pypylon import pylon
 import cv2
 
-# Basler 카메라를 검색합니다.
+# Basler 카메라를 검색
 devices = pylon.TlFactory.GetInstance().EnumerateDevices()
 if len(devices) == 0:
     print("카메라가 발견되지 않았습니다.")
     exit(1)
 
-# 첫 번째 카메라를 선택합니다.
+# 첫 번째 카메라를 선택
 camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateDevice(devices[0]))
-
-# 카메라의 속성을 설정합니다.
 camera.Open()
+
+# 카메라 환경설정
 camera.Width.Value = camera.Width.Max
 camera.Height.Value = camera.Height.Max
 camera.BalanceWhiteAuto.SetValue("Continuous") # 자동 WB 설정
 camera.GainAuto.SetValue('Off')
 camera.GainRaw.Value = 200
 
-# 이미지를 취득합니다.
+# 카메라에서 영상 캡처
 camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
 converter = pylon.ImageFormatConverter()
 converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+
+# 화면에 영상 표시
 while camera.IsGrabbing():
-    grab_result = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
-    if grab_result.GrabSucceeded():
-        # 이미지를 OpenCV 형식으로 변환합니다.
-        image = converter.Convert(grab_result)
-        img = image.GetArray()
-        # 이미지를 축소합니다.
-        img_resized = cv2.resize(img, None, fx=0.1, fy=0.1) # 0.5배 축소
-        # 이미지를 표시합니다.
-        cv2.imshow("Basler Camera", img_resized)
-        cv2.waitKey(1)
-    grab_result.Release()
+    grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+    if grabResult.GrabSucceeded():
+        image = converter.Convert(grabResult)
+        frame = image.GetArray()
+        frame_resized = cv2.resize(frame, None, fx=0.1, fy=0.1) # 0.9배 축소
+        cv2.imshow("Camera", frame_resized)
+    grabResult.Release()
     
-# 카메라를 정리합니다.
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# 정리
 camera.StopGrabbing()
 camera.Close()
 cv2.destroyAllWindows()
--- test.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-
-# 이미지 출력
-plt.imshow(img)
-plt.show()
